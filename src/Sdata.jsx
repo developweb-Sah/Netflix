5b4809daa05a57345628596edaf4a068401006ae
SaheenJasmine <63956055+developweb-Sah@users.noreply.github.com>
2021-05-10 22:27:53 +0530

    Netflix Page

---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata=[
+    {
+        id:1,
+        sname: "DARK",
+        imgsrc: "https://wallpapercave.com/wp/wp5243011.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80100172",
+    },
+    {
+        id:2,
+        sname: "Extra Curriculam",
+        imgsrc: "https://wallpapercave.com/wp/wp6329676.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80990668",
+    },
+    {
+        id:3,
+        sname: "A Million LittleThings",
+        imgsrc: "https://static0.therichestimages.com/wordpress/wp-content/uploads/2019/11/A-million-little-things.jpg",
+        title: "A Netflix Original Series",
+        link: "https://tvline.com/2021/05/05/a-million-little-things-recap-season-3-episode-12-junior/",
+    },
+    {
+        id:4,
+        sname: "THE RAIN",
+        imgsrc: "https://cdn.wallpapersafari.com/68/99/9ajkwv.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80154610",
+    },
+    {
+        id:5,
+        sname: "THE RAIN",
+        imgsrc: "https://cdn.wallpapersafari.com/68/99/9ajkwv.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80154610",
+    },
+];
+
+export default Sdata;
